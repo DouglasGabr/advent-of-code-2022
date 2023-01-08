a57bd5a5adcf48cd7f0704eead4a51651eedbdb7
DouglasGabr <douglasgabriel99@gmail.com>
2023-01-07 23:45:29 -0300

    day 14 part 2

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -6,12 +6,23 @@ use std::{
 fn main() {
     let input = include_str!("../input/day14/prod.txt");
     println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
 }
 
 fn part1(input: &str) -> u32 {
     let mut map = Map::from(input);
     loop {
-        if !map.drop_sand_unit() {
+        if !map.drop_sand_unit(false) {
+            break;
+        }
+    }
+    map.get_number_of_sand_at_rest()
+}
+
+fn part2(input: &str) -> u32 {
+    let mut map = Map::from(input);
+    for _ in 0.. {
+        if !map.drop_sand_unit(true) {
             break;
         }
     }
@@ -38,6 +49,7 @@ impl Display for Tile {
 
 struct Map {
     tiles: HashMap<(u32, u32), Tile>,
+    floor_level: u32,
 }
 
 impl Map {
@@ -51,23 +63,26 @@ impl Map {
             .count() as u32
     }
 
-    fn drop_sand_unit(&mut self) -> bool {
-        let mut sand_position = (500, 0);
-        let max_y = self.tiles.iter().map(|tile| tile.0 .0).max().unwrap();
-        'falling: while sand_position.1 <= max_y {
-            let possible_positions = [
-                (sand_position.0, sand_position.1 + 1),
-                (sand_position.0 - 1, sand_position.1 + 1),
-                (sand_position.0 + 1, sand_position.1 + 1),
-            ];
-            for possible_position in possible_positions {
-                if !self.tiles.contains_key(&possible_position) {
-                    sand_position = possible_position;
-                    continue 'falling;
+    fn drop_sand_unit(&mut self, has_floor: bool) -> bool {
+        let first_y_hit = self.tiles.iter().map(|tile| tile.0 .1).min().unwrap();
+        let mut sand_position = (500, first_y_hit - 1);
+        'falling: while sand_position.1 <= self.floor_level {
+            let new_y = sand_position.1 + 1;
+            if !(has_floor && new_y == self.floor_level) {
+                let possible_positions = [
+                    (sand_position.0, new_y),
+                    (sand_position.0 - 1, new_y),
+                    (sand_position.0 + 1, new_y),
+                ];
+                for possible_position in possible_positions {
+                    if !self.tiles.contains_key(&possible_position) {
+                        sand_position = possible_position;
+                        continue 'falling;
+                    }
                 }
             }
             self.tiles.insert(sand_position, Tile::Sand);
-            return true;
+            return sand_position != (500, 0);
         }
         false
     }
@@ -95,32 +110,34 @@ impl Display for Map {
 
 impl From<&str> for Map {
     fn from(value: &str) -> Self {
+        let tiles: HashMap<(u32, u32), Tile> = value
+            .lines()
+            .flat_map(|line| {
+                let path: Vec<(u32, u32)> = line
+                    .split(" -> ")
+                    .filter_map(|coords| coords.split_once(','))
+                    .map(|(x, y)| {
+                        (
+                            x.parse::<u32>().expect("valid x"),
+                            y.parse::<u32>().expect("valid y"),
+                        )
+                    })
+                    .collect();
+                path.windows(2)
+                    .flat_map(|path_slice| {
+                        let (x1, y1) = path_slice[0];
+                        let (x2, y2) = path_slice[1];
+                        let (x1, x2) = (x1.min(x2), x1.max(x2));
+                        let (y1, y2) = (y1.min(y2), y1.max(y2));
+                        (x1..=x2).flat_map(move |x| (y1..=y2).map(move |y| (x, y)))
+                    })
+                    .collect::<HashSet<(u32, u32)>>()
+            })
+            .map(|coord| (coord, Tile::Rock))
+            .collect();
         Map {
-            tiles: value
-                .lines()
-                .flat_map(|line| {
-                    let path: Vec<(u32, u32)> = line
-                        .split(" -> ")
-                        .filter_map(|coords| coords.split_once(','))
-                        .map(|(x, y)| {
-                            (
-                                x.parse::<u32>().expect("valid x"),
-                                y.parse::<u32>().expect("valid y"),
-                            )
-                        })
-                        .collect();
-                    path.windows(2)
-                        .flat_map(|path_slice| {
-                            let (x1, y1) = path_slice[0];
-                            let (x2, y2) = path_slice[1];
-                            let (x1, x2) = (x1.min(x2), x1.max(x2));
-                            let (y1, y2) = (y1.min(y2), y1.max(y2));
-                            (x1..=x2).flat_map(move |x| (y1..=y2).map(move |y| (x, y)))
-                        })
-                        .collect::<HashSet<(u32, u32)>>()
-                })
-                .map(|coord| (coord, Tile::Rock))
-                .collect(),
+            floor_level: tiles.keys().map(|tile| tile.1).max().unwrap() + 2,
+            tiles,
         }
     }
 }
@@ -133,5 +150,11 @@ mod tests {
     fn test_part1() {
         let input = include_str!("../input/day14/test.txt");
         assert_eq!(part1(input), 24);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = include_str!("../input/day14/test.txt");
+        assert_eq!(part2(input), 93);
     }
 }
