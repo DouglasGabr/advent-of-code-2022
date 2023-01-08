a783395745ec1bc246b2e6e0a6026ba908117928
DouglasGabr <douglasgabriel99@gmail.com>
2023-01-08 09:08:55 -0300

    wip day 15

---
+++ src/bin/day15.rs
@@ -0,0 +1,19 @@
+fn main() {}
+
+fn part1() -> u32 {
+    todo!()
+}
+
+fn part2() -> u32 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(), 26);
+    }
+}
